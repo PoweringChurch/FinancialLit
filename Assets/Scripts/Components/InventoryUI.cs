745cb627a6bb7a988d94e953bd7cb5cda328eb7d
PoweringChurch <coolyoda4@gmail.com>
2025-10-31 10:22:35 -0400

    Update InventoryUI.cs
    added function to ad

--- Assets/Scripts/Components/InventoryUI.cs
+++ Assets/Scripts/Components/InventoryUI.cs
@@ -7,6 +7,8 @@ public class InventoryUI : MonoBehaviour
     public Transform contentTransform;
     public Inventory inventory;
     public ObjectPlacer objectPlacer;
+
+    private Dictionary<string, GameObject> inventoryItemUI = new();
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -29,7 +31,22 @@ public class InventoryUI : MonoBehaviour
             displayNameText.GetComponent<TextMeshProUGUI>().text = item.displayName;
             countText.GetComponent<TextMeshProUGUI>().text = $"{item.Count}";
             itemButton.onClick.AddListener(() => OnItemButtonClicked(item));
+            inventoryItemUI.Add(item.displayName, newTemplate);
         }
+    }
+    public void UpdateInventoryItem(string itemname)
+    {
+        InventoryItem item = Inventory.GetItem(itemname);
+        GameObject itemUi = inventoryItemUI[itemname];
+        if (item == null)
+        {
+            Debug.Log("THERE IS NO ITEM AHHH AHHHHHH");
+            Destroy(itemUi); //no idea if this works?
+            return;
+        }
+        Transform countText = newTemplate.transform.Find("Count");
+        countText.GetComponent<TextMeshProUGUI>().text = $"{item.Count}";
+        
     }
     private void OnItemButtonClicked(InventoryItem item)
     {
