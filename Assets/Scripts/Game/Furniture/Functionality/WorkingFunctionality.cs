9cae6454874829835fa8c01a4d18ee5ce0f4ab3c
Mimic-Gamer <IraNeygithub@gmail.com>
2026-01-27 09:59:04 -0500

    Added some comments
    Added comments to some of the scripts

--- Assets/Scripts/Game/Furniture/Functionality/WorkingFunctionality.cs
+++ Assets/Scripts/Game/Furniture/Functionality/WorkingFunctionality.cs
@@ -5,6 +5,7 @@ public class WorkingFunctionality : BaseFunctionality
         base.Awake();
         homeActions["Go to work"] = BeginWork;
     }
+    //Begin work when go to work is called upon
     protected void BeginWork()
     {
         UIWorkManager.Instance.EnterWork();
